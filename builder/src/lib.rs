db78f4d28d2455dddb122cd994d5caa1c6b02858
haiyang <phycgp@gmail.com>
2024-03-14 11:54:11 +0800

    builder test02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,32 @@
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse_macro_input};
-
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    
-    TokenStream::new()
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let expanded = quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        // The generated impl.
+        impl #impl_generics #name #ty_generics #where_clause {
+            fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
