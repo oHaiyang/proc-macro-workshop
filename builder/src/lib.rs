123cf80a44bb5c0d229beba5e934d3300bac7b8b
haiyang <phycgp@gmail.com>
2024-03-14 10:31:23 +0800

    builder test01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use syn::{DeriveInput, parse_macro_input};
+
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _ = parse_macro_input!(input as DeriveInput);
+    
+    TokenStream::new()
 }
